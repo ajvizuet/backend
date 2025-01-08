686f1b27958d64c08bef50585a50eb91e6389be2
= <=>
2025-01-08 11:17:42 -0500

    Landing Detail API implemented

--- backend/api/views.py
+++ backend/api/views.py
@@ -30,3 +30,45 @@ class LandingAPI(APIView):
         new_resource = ref.push(request.data)
 
         return Response({"id": new_resource.key}, status=status.HTTP_201_CREATED)
+    
+class LandingAPIDetail(APIView):
+    name = 'Landing Detail API'
+
+    collection_name = 'collection'
+
+    def get(self, request, pk):
+        ref = db.reference(f'{self.collection_name}/{pk}')
+
+        item = ref.get()
+        if item:
+            return Response(item, status=status.HTTP_200_OK)
+        else:
+            return Response("Item not found", status.HTTP_404_NOT_FOUND)
+
+    
+    def put(self, request, pk):
+        ref = db.reference(f'{self.collection_name}/{pk}')
+
+        if request.data:
+            current_time = datetime.now()
+            custom_format = current_time.strftime("%d/%m/%Y, %I:%M:%S %p").lower().replace('am', 'a. m.').replace('pm', 'p. m.')
+            request.data.update({"saved": custom_format})
+
+            if ref.get():
+                update = ref.update(request.data)
+
+                return Response("Item updated", status=status.HTTP_200_OK)
+            else:
+                return Response("Item not found", status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response("Missing data", status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, pk):
+        ref = db.reference(f'{self.collection_name}/{pk}')
+
+        if ref.get():
+            ref.delete()
+            return Response("Item deleted", status=status.HTTP_204_NO_CONTENT)
+
+        return Response("Item not found", status=status.HTTP_404_NOT_FOUND)
