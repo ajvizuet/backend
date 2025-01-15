39e5ea275e01384cbc39bada2f3109222feea55b
= <=>
2025-01-15 10:09:23 -0500

    Added Auth and Admin apps

--- backend/main/views.py
+++ backend/main/views.py
@@ -3,5 +3,38 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from django.contrib.auth.decorators import login_required, permission_required
+
+import requests
+import json
+
+@login_required
+@permission_required('main.index_viewer', raise_exception=True)
 def index(request):
-    return render(request, 'main/index.html')
+    
+    # Arme el endpoint del REST API
+    current_url = request.build_absolute_uri()
+    url = current_url + '/api/v1/landing'
+
+    # Petición al REST API
+    response_http = requests.get(url)
+    response_dict = json.loads(response_http.content)
+
+    print("Endpoint ", url)
+    print("Response ", response_dict)
+
+    # Respuestas totales
+    total_responses = len(response_dict.keys())
+
+    # Objeto con los datos a renderizar
+    responses = response_dict.values()
+
+     # Objeto con los datos a renderizar
+    data = {
+        'title': 'Landing - Dashboard',
+        'total_responses': total_responses,
+        'responses': responses
+    }
+
+    # Renderización en la plantilla
+    return render(request, 'main/index.html', data)
--- backend/static/js/charts-pie.js
+++ backend/static/js/charts-pie.js
@@ -1,6 +1,7 @@
 /**
  * For usage, visit Chart.js docs https://www.chartjs.org/docs/latest/
  */
+
 const pieConfig = {
   type: 'doughnut',
   data: {
@@ -15,7 +16,7 @@ const pieConfig = {
         label: 'Dataset 1',
       },
     ],
-    labels: ['Shoes', 'Shirts', 'Bags'],
+    labels: ['Bags', 'Shirts', 'Bags'],
   },
   options: {
     responsive: true,
@@ -33,3 +34,58 @@ const pieConfig = {
 // change this to the id of your chart element in HMTL
 const pieCtx = document.getElementById('pie')
 window.myPie = new Chart(pieCtx, pieConfig)
+
+countCommentsByHour = (data) => {
+  const labels = ["0 a.m. - 8 a.m.", "8 a.m. - 16 p.m.", "16 p.m. - 0 a.m."];
+  const counts = [0, 0, 0];
+
+  Object.values(data).forEach(record => {
+    const savedTime = record.saved;
+
+    console.log(record);
+
+    if(!savedTime) {
+      return;
+    }
+
+    const formattedTime = savedTime.replace('a. m.', 'AM').replace('p. m.', 'PM');
+       // Crear objeto Date con la cadena de tiempo
+       const dt = new Date(Date.parse(formattedTime.replace(/(\d{2}\/\d{2}\/\d{4}), (\d{2}):(\d{2}):(\d{2}) (AM|PM)/, '$1 $2:$3:$4 $5')));
+       const hour = dt.getHours();
+
+       // Clasificar en el rango correspondiente
+       if (hour >= 0 && hour < 8) {
+           counts[0]++;
+       } else if (hour >= 8 && hour < 16) {
+           counts[1]++;
+       } else {
+           counts[2]++;
+       }
+   });
+
+   return { labels, counts };
+};
+
+update = () => {
+  fetch('/api/v1/landing')
+    .then(response => response.json())
+    .then(data => {
+
+      let { labels, counts } = countCommentsByHour(data)
+
+      // Reset data
+      window.myPie.data.labels = [];
+      window.myPie.data.datasets[0].data = [];
+
+      // New data
+      window.myPie.data.labels = labels
+      window.myPie.data.datasets[0].data = counts
+
+      window.myPie.update();
+
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+update();
+
